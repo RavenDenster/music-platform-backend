2cd9aa1dec178ef3d05561cb7d597d68167355ac
Oleg Kashechkin <95638297+RavenDenster@users.noreply.github.com>
2023-04-19 17:26:31 +0300

    change path

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -1,7 +1,7 @@
 import {Injectable, HttpException, HttpStatus} from "@nestjs/common"
-import path from 'path'
-import fs from 'fs'
-import uuid from 'uuid'
+import * as path from 'path'
+import * as fs from 'fs'
+import * as uuid from 'uuid'
 
 export enum FileType {
     AUDIO = 'audio',
@@ -14,14 +14,14 @@ export class FileService{
         try {
             const fileExtension = file.originalname.split('.').pop()
             const fileName = uuid.v4() + '.' + fileExtension
-            const filePath = path.resolve(__dirname, '..', 'static', type)
+            const filePath = path.resolve(__dirname, '..', 'static')
             
             if(!fs.existsSync(filePath)) {
                 fs.mkdirSync(filePath, {recursive: true})
             }
             fs.writeFileSync(path.resolve(filePath, fileName), file.buffer)
 
-            return type + '/' + fileName
+            return fileName
         } catch (e) {
             throw new HttpException(e.message, HttpStatus.INTERNAL_SERVER_ERROR)
         }
