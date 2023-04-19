e760183e83287899c290d5b93fd5fadf9d1b3792
Oleg Kashechkin <95638297+RavenDenster@users.noreply.github.com>
2023-04-19 21:08:22 +0300

    change path

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -14,7 +14,7 @@ export class FileService{
         try {
             const fileExtension = file.originalname.split('.').pop()
             const fileName = uuid.v4() + '.' + fileExtension
-            const filePath = path.resolve(__dirname, '..', 'static', type)
+            const filePath = path.resolve(__dirname, '..',)
             
             if(!fs.existsSync(filePath)) {
                 fs.mkdirSync(filePath, {recursive: true})
