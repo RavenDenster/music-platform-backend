ad8d88038f1e37a39c9b8d80ff1e214dd51508a8
Oleg Kashechkin <95638297+RavenDenster@users.noreply.github.com>
2023-04-19 22:46:45 +0300

    body-parser

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
 import cookieParser from 'cookie-parser';
+import * as bodyParser from 'body-parser'
 
 const start = async () => {
     try {
         const PORT = process.env.PORT || 5000
         const app = await NestFactory.create(AppModule)
+        app.use(bodyParser.json({limit: '50mb'}))
+        app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
         app.enableCors({
             origin: [
               'http://localhost:3000',
