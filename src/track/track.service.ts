4e4bb883fe188e92e775190b9dab44dfeecdc9d0
Oleg Kashechkin <95638297+RavenDenster@users.noreply.github.com>
2023-04-19 22:59:02 +0300

    import

--- src/track/track.service.ts
+++ src/track/track.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from "@nestjs/common"
 import { InjectModel } from '@nestjs/mongoose'
 import { Model, ObjectId } from "mongoose"
-import { FileService, FileType } from "src/file/file.service"
+import { FileService, FileType } from "../file/file.service"
 import { CreateCommentDto } from "./dto/create-comments.dto"
 import { CreateTrackDto } from "./dto/create-track.dto"
 import { Comment, CommentDocument } from "./schemas/comment.schema"
