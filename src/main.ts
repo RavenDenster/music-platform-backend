971d0a15aa4377b1c7476840e58bcf7d610794bd
Oleg Kashechkin <95638297+RavenDenster@users.noreply.github.com>
2023-04-22 18:00:12 +0300

    headers

--- src/main.ts
+++ src/main.ts
@@ -7,20 +7,7 @@ const start = async () => {
     try {
         const PORT = process.env.PORT || 5000
         const app = await NestFactory.create(AppModule)
-        app.enableCors({
-            origin: [
-              'https://music-platform-frontend-zeta.vercel.app',
-              // 'http://localhost:3000',
-              'http://example.com',
-              'http://www.example.com',
-              'http://app.example.com',
-              'https://example.com',
-              'https://www.example.com',
-              'https://app.example.com',
-            ],
-            methods: ["GET", "POST"],
-            credentials: true,
-          })
+        app.enableCors()
         app.use(cookieParser())
         await app.listen(PORT, () => console.log(`server started on PORT ${PORT}`))
     } catch (e) { 
