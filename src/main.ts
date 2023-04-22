d952c74fcb543f0c897915d4f415b40fe00489d5
Oleg Kashechkin <95638297+RavenDenster@users.noreply.github.com>
2023-04-22 19:48:56 +0300

    cors-13

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,28 @@ import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
 import cookieParser from 'cookie-parser';
 
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+const handler = (req, res) => {
+  const d = new Date()
+  res.end(d.toString())
+}
+
 const start = async () => {
     try {
         const PORT = process.env.PORT || 5000
@@ -13,7 +35,8 @@ const start = async () => {
             ],
             methods: ["GET", "POST", "PUT", "DELETE"],
             credentials: true,
-          })
+        })
+        app.use(allowCors(handler))
         app.use(cookieParser())
         await app.listen(PORT, () => console.log(`server started on PORT ${PORT}`))
     } catch (e) { 
