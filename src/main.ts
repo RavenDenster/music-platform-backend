34a6f19e0109ba741504a3df4560ab6f8cd00b08
Oleg Kashechkin <95638297+RavenDenster@users.noreply.github.com>
2023-04-22 18:35:06 +0300

    cors-7

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,25 @@
 import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
 import cookieParser from 'cookie-parser';
-const cors = require('cors');
-const corsOptions = {
-  origin:'https://music-platform-frontend-zeta.vercel.app', 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200
-}
 
 const start = async () => {
     try {
         const PORT = process.env.PORT || 5000
         const app = await NestFactory.create(AppModule)
-        // app.enableCors({
-        //     origin: [
-        //       'https://music-platform-frontend-zeta.vercel.app',
-        //       // 'http://localhost:3000',
-        //       'http://example.com',
-        //       'http://www.example.com',
-        //       'http://app.example.com',
-        //       'https://example.com',
-        //       'https://www.example.com',
-        //       'https://app.example.com',
-        //     ],
-        //     methods: ["GET", "POST"],
-        //     credentials: true,
-        //   })
-        app.use(cors(corsOptions));
+        app.enableCors({
+            origin: [
+              // 'https://music-platform-frontend-zeta.vercel.app',
+              'http://localhost:3000',
+              'http://example.com',
+              'http://www.example.com',
+              'http://app.example.com',
+              'https://example.com',
+              'https://www.example.com',
+              'https://app.example.com',
+            ],
+            methods: ["GET", "POST", "PUT", "DELETE"],
+            credentials: true,
+          })
         app.use(cookieParser())
         await app.listen(PORT, () => console.log(`server started on PORT ${PORT}`))
     } catch (e) { 
